020fa2b21a0a102056b5d50d016a107604d25ccb
Lea Dalibard <leadalibard@gmail.com>
2021-01-13 15:36:39 +0100

    Addind some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'title': ['Architects Daughter', 'cursive'],
+            },
+            colors:{
+                background :'#EFE2BA',
+                ocre:'#EFE2BA',
+                red:'#F13C20',
+                dkBlue:'#4056A1',
+                lgBlue:'#C5CBE3',
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
